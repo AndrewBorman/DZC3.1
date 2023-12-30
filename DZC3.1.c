dd77fe87f60cc2e8475ad5c9d1c870aefb38772c
Andrew <abaravy@gmail.com>
2023-12-30 19:16:53 +0300

    DZC3.1

---
+++ DZC3.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int k, l, m, sum;
+
+int main()
+{
+    printf("Wwedite tri 4isla\n");
+    scanf("%d %d %d", &k, &l, &m);
+    sum = k+l+m;
+    printf("%d + %d + %d = %d", k, l, m, sum);
+
+    
+    return 0;
+}
